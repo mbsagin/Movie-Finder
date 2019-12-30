a543a77299ca4e272ae085aca9f0546bbdc398f7
Mehmet Beraat Sağın <mbsagin@gmail.com>
2019-12-30 15:54:02 +0300

    Add project files.

---
+++ HW-2/MovieFinder.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="MovieFinder.aspx.cs" Inherits="HW_2.WebForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Movie Finder</title>
+    <style>
+        h1, h3, p {
+            display: inline;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>     
+            <h1>Enter a movie : </h1><input type="text" name="Title" id="movieTitle" runat="server" /><br />
+            <h3>Movie Title : </h3>
+            <p id="ttl" runat="server">-</p><br />
+            <h3>Year : </h3>
+            <p id="year" runat="server"></p><br />
+            <h3>Released Date : </h3>
+            <p id="release" runat="server">-</p><br />
+            <h3>Genre : </h3>
+            <p id="genre" runat="server">-</p><br />
+            <h3>Director : </h3>
+            <p id="director" runat="server">-</p><br />
+            <h3>Actors : </h3>
+            <p id="actors" runat="server">-</p><br />
+            <h3>Language : </h3>
+            <p id="lang" runat="server">-</p><br />
+            <h3>Country : </h3>
+            <p id="country" runat="server">-</p><br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ HW-2/MovieFinder.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Newtonsoft.Json;
+
+namespace HW_2
+{
+    public partial class WebForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string key = "952eb1f3";
+            string Url = $"http://www.omdbapi.com/?apikey={key}"; //copy your address
+            string jsonData;
+            string title = movieTitle.Value;
+
+            var fullUrl = new StringBuilder(Url);
+            fullUrl.Append($"&t={title}");
+
+            HttpWebRequest request = WebRequest.Create(fullUrl.ToString()) as HttpWebRequest;
+            using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+            {
+                StreamReader reader = new StreamReader(response.GetResponseStream());
+                jsonData = reader.ReadToEnd();
+            }
+            var result = JsonConvert.DeserializeObject<Movie>(jsonData);
+            
+            ttl.InnerHtml = result.Title;
+            year.InnerHtml = result.Year;
+            release.InnerHtml = result.Released;
+            genre.InnerHtml = result.Genre;
+            director.InnerHtml = result.Director;
+            actors.InnerHtml = result.Actors;
+            lang.InnerHtml = result.Language;
+            country.InnerHtml = result.Country;
+        }
+    }
+}
